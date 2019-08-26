c390425cfb5b5d08e97f24187852c006b23b3bf0
liulin <liulin@qq.com>
2019-08-26 17:27:04 +0800

    1111

--- src/main/resources/webapp/src/ui/layui/extends/extend.js
+++ src/main/resources/webapp/src/ui/layui/extends/extend.js
@@ -1,7 +1,8 @@
 layui.extend({
     global:'{/}/ui/layui/extends/global',  //公用方法
     jutil:'{/}/ui/layui/extends/jsutil',  //工具(json处理等)
-    dictpl:'{/}/ui/layui/extends/dictpl'  //字典显示处理
-}).define(['global','jutil','dictpl'],function (e) {
+    dictpl:'{/}/ui/layui/extends/dictpl',  //字典显示处理
+    dtree:'{/}/ui/layui/extends/dtree/dtree'  //字典显示处理
+}).define(['global','jutil','dictpl','dtree'],function (e) {
     e('exd',function(){})
 })
